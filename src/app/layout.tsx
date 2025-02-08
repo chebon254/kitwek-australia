da287900a3f667fa1e920f520b2d49f760cd8257
chebon254 <kelvinchebon90@gmail.com>
2025-02-09 00:39:06 +0300

    Font Change Ubuntu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
 import "./globals.css";
+
+const ubuntu = Ubuntu({
+  variable: "--font-ubuntu-sans",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"]
+});
 
 export const metadata: Metadata = {
   title: "Kitwek South Australia - Strengthening the Kalenjin Community",
-  description: "Kitwek South Australia fosters cultural preservation, social empowerment, and economic advancement for the Kalenjin community in Victoria, Australia. Join us in promoting heritage, education, and integration within Australian society.",
+  description:
+    "Kitwek South Australia fosters cultural preservation, social empowerment, and economic advancement for the Kalenjin community in Victoria, Australia. Join us in promoting heritage, education, and integration within Australian society.",
 };
 
 export default function RootLayout({
@@ -14,16 +22,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
         <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
         <meta name="apple-mobile-web-app-title" content="Kitwek Australia" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
-      <body>
-        {children}
-      </body>
+      <body  className={`${ubuntu.variable} antialiased`}>{children}</body>
     </html>
   );
 }
