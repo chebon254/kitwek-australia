be3d36e87e0f2481b4221e55facd7e63bfb055e2
chebon254 <kelvinchebon90@gmail.com>
2025-02-10 16:14:59 +0300

    member success

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -58,9 +58,10 @@ export default function SignUp() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Create your account
+        <h2 className="mt-6 mb-2 text-center text-3xl font-extrabold text-gray-900">
+          Member Registration
         </h2>
+        <p className="mb-10 text-center text-base font-normal text-gray-900">Create an account by registering</p>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
