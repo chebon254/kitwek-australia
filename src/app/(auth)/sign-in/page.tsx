996ba6909fd4406467053a67275ec5a07a68888c
chebon254 <kelvinchebon90@gmail.com>
2025-02-22 04:21:52 +0300

    image show

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -181,7 +181,7 @@ function SignIn() {
                 className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <Image
-                  src="/ui-assets/google-icon.png"
+                  src={"/ui-assets/google-icon.png"}
                   alt="Google"
                   width={20}
                   height={20}
@@ -196,7 +196,7 @@ function SignIn() {
                 className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
               >
                 <Image
-                  src="/ui-assets/apple-icon.png"
+                  src={"/ui-assets/apple-icon.png"}
                   alt="Apple"
                   width={20}
                   height={20}
