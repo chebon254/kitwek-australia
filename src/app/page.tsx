8b6df06d410b8e18928ebaddf3eab8662f3aa13f
chebon254 <kelvinchebon90@gmail.com>
2025-02-24 15:23:26 +0300

    ixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -309,7 +309,7 @@ export default function Home() {
         </div>
 
         {/* Team Section */}
-        {/* <div className="w-full bg-white py-16" ref={teamRef}>
+        <div className="w-full bg-white py-16" ref={teamRef}>
           <motion.div
             className="text-center mb-10"
             variants={fadeInUp}
@@ -385,7 +385,7 @@ export default function Home() {
               </div>
             </motion.div>
           </div>
-        </div> */}
+        </div>
       </main>
       <Footer />
     </>
