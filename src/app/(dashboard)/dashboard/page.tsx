1b72d7f1d8b1ea418405dbfa36cb7ebd1699ce25
chebon254 <kelvinchebon90@gmail.com>
2025-03-12 18:54:49 +0300

    changes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -236,15 +236,6 @@ export default function Dashboard() {
                         {getSubscriptionButtonText()}
                       </Link>
                     )}
-                  {data.user.subscription === "Premium" &&
-                    data.user.membershipStatus === "ACTIVE" && accountAge >= 365 &&(
-                      <Link
-                        href="/dashboard/subscription"
-                        className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                      >
-                        Manage Subscription
-                      </Link>
-                    )}
                 </div>
               </div>
             </div>
